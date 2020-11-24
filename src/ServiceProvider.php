e85a0d3b193a20fd397a7ee036ed123754a01cd5
Alexey Filippov <sobsgm@gmail.com>
2020-11-25 01:08:34 +0300

    WIP

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -6,9 +6,18 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
     public function register()
     {
-        $configPath = __DIR__ . '/../config/smsc.php';
-        $this->mergeConfigFrom($configPath, 'smsc');
-
+        $this->mergeConfigFrom($this->getConfigPath(), 'smsc');
         $this->app->alias(SMSC::class, 'smsc');
+    }
+
+    public function boot()
+    {
+        $configPath = __DIR__ . '/../config/smsc.php';
+        $this->publishes([$configPath => $this->getConfigPath()], 'config');
+    }
+
+    protected function getConfigPath(): string
+    {
+        return __DIR__ . '/../config/smsc.php';
     }
 }
